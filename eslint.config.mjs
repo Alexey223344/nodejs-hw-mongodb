5a618e815a2f7f83c842d045411cd1ad4a00cab0
Alexey <mirar2246@gmail.com>
2024-11-23 21:30:55 +0200

    akl

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,14 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
+import globals from 'globals';
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
-  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
+  {
+    files: ['src/**/*.js'],
+    languageOptions: { globals: globals.node },
+    rules: {
+      semi: 'error',
+      'no-unused-vars': ['error', { args: 'none' }],
+      'no-undef': 'error',
+    },
+  },
 ];
