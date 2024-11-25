d73f7c51dc15eae68d231163ca802660dc3220b4
Alexey <mirar2246@gmail.com>
2024-11-25 15:35:31 +0200

    addddd

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -4,12 +4,12 @@ import { env } from '../utils/env.js';
 export const initMongoConnection = async () => {
   try {
     const user = env('MONGODB_USER');
-    const pswd = env('MONGODB_PASSWORD');
+    const pwd = env('MONGODB_PASSWORD');
     const url = env('MONGODB_URL');
     const db = env('MONGODB_DB');
 
     await mongoose.connect(
-      `mongodb+srv://${user}:${pswd}@${url}/${db}?retryWrites=true&w=majority&appName=univercity`,
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=univercity`,
     );
 
     console.log('Mongo connection successfully established!');
