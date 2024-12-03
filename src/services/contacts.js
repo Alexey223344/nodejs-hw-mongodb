2aafb750e93d72d4e3c75a2c7ba9b94ed6397361
Alexey <mirar2246@gmail.com>
2024-12-03 17:47:44 +0200

    crud

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -9,3 +9,27 @@ export const getContactById = async (contactId) => {
   const contact = await ContactsAll.findById(contactId);
   return contact;
 };
+
+export const createContact = async (payload) => {
+  const contact = await ContactsAll.create(payload);
+  return contact;
+};
+
+export const updateContact = async (contactId, contact, options = {}) => {
+  const rawResult = await ContactsAll.findByIdAndUpdate(contactId, contact, {
+    new: true,
+    includeResultMetadata: true,
+    ...options,
+  });
+
+  return !rawResult || !rawResult.value
+    ? null
+    : {
+        contact: rawResult.value,
+        isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+      };
+};
+
+export const deleteContact = (contactId) => {
+  return ContactsAll.findByIdAndDelete(contactId);
+};
